93deac45c52edd340f14e2521ffc6ba240906423
quietsato <quietsato@gmail.com>
2021-11-02 19:17:08 +0900

    Add project files

---
+++ src/main/java/io/github/quietsato/myplugin/MyPlugin.kt
@@ -0,0 +1,13 @@
+package io.github.quietsato.myplugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MyPlugin : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
