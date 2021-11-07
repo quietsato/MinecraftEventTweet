06539d671e5da132de42a6355a0949eb8327ed1d
quietsato <quietsato@gmail.com>
2021-11-07 16:42:31 +0900

    Catch player death event

--- src/main/java/io/github/quietsato/myplugin/MyPlugin.kt
+++ src/main/java/io/github/quietsato/myplugin/MyPlugin.kt
@@ -1,13 +1,26 @@
 package io.github.quietsato.myplugin
 
+import org.bukkit.entity.EntityType
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDeathEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class MyPlugin : JavaPlugin() {
+class MyPlugin : JavaPlugin(), Listener {
     override fun onEnable() {
-        // Plugin startup logic
+        logger.info("Plugin activated")
+
+        server.pluginManager.registerEvents(this, this)
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        logger.info("Plugin activated")
+    }
+
+    @EventHandler
+    fun onPlayerDied(e: EntityDeathEvent) {
+        if (e.entityType != EntityType.PLAYER) return
+
+        logger.info("Player '${e.entity.name}' died")
     }
 }
