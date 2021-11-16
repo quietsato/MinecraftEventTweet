6a377c685c2d7a0c9005c653236d2ad13e939eeb
quietsato <quietsato@gmail.com>
2021-11-16 20:22:54 +0900

    Tweet death message

--- src/main/java/io/github/quietsato/minecraft_event_tweet/MinecraftEventTweet.kt
+++ src/main/java/io/github/quietsato/minecraft_event_tweet/MinecraftEventTweet.kt
@@ -3,10 +3,9 @@ package io.github.quietsato.minecraft_event_tweet
 import io.github.quietsato.minecraft_event_tweet.twitter.TwitterManager
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.entity.EntityDeathEvent
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 
@@ -43,23 +42,22 @@ class MinecraftEventTweet : JavaPlugin(), Listener {
         if (args[0].lowercase() == "tweet") {
             if (args.size < 2) return false
 
-            val tweetContent = args[1]
+            val tweetContent = args.asSequence().drop(1).joinToString(" ")
 
             twitterManager?.also { tm ->
                 tm.tweetString(tweetContent)
+                logger.info("Tweeted: $tweetContent")
                 return true
             }
-            logger.warning("TwitterManager is not initialised")
+            logger.warning("TwitterManager is not initialized")
             return false
         }
         return false
     }
 
     @EventHandler
-    fun onPlayerDeath(e: EntityDeathEvent) {
-        if (e.entityType != EntityType.PLAYER) return
-
-        logger.info("Player '${e.entity.name}' died")
+    fun onPlayerDeath(e: PlayerDeathEvent) {
+        twitterManager?.tweetString(e.deathMessage)
     }
 
     private fun initTwitterManager(): TwitterManager? {
